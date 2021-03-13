0dfe5b8ef8692da06a59962980471ca363792833
FarinHeiT <yariksuharik85@gmail.com>
2021-03-13 19:55:41 +0200

    routes with parameter parser

--- api.py
+++ api.py
@@ -1,4 +1,5 @@
 from webob import Request, Response
+from parse import parse
 
 class AppFactory:
     def __init__(self):
@@ -17,15 +18,33 @@ class AppFactory:
         request = Request(environ)
 
         response = self.handle_request(request)
-        
+
         return response(environ, start_response)
 
+
+    def default_response(self, response):
+        response.status_code = 404
+        response.text = "Not Found"
+
     
+    def get_handler(self, request_route):
+        for path, handler in self.routes.items():
+            parse_params = parse(path, request_route)
+            if parse_params:
+                return handler, parse_params.named
+        
+        return None, None
+
+
     def handle_request(self, request):
         response = Response()
+        
+        handler, params = self.get_handler(request.path)
 
-        for path, handler in self.routes.items():
-            if path == request.path:
-                handler(request, response)
-                
-                return response
+        if handler:
+            handler(request, response, **params)
+        else:
+            self.default_response(response)
+
+        return response
+        
--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from api import AppFactory
 
+
 app = AppFactory()
 
-@app.route('/home')
+@app.route('/home/')
 def home(request, response):
     response.text = "Hello from the HOME page"
 
@@ -10,3 +11,8 @@ def home(request, response):
 @app.route('/about')
 def about(request, response):
     response.text = "Hello from the ABOUT page"
+
+
+@app.route('/passdigit/{digit:d}')
+def pass_digit(request, response, digit):
+    response.text = str(digit)
